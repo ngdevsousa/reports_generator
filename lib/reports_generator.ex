47410c2a31e02fe5a36bb2f0df7de2a3f7a3f3f4
f.sousa <ng.dev.sousa@outlook.com>
2021-03-11 20:09:19 -0300

    Refactor modules structure

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,20 +1,13 @@
+alias ReportsGenerator.Parser
+
 defmodule ReportsGenerator do
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.reduce(report_acc(), fn line, report ->
-      [id, _dish, price] = parse_line(line)
-      Map.put(report, id, price + report[id])
-    end)
-    # |> Enum.reduce(%{}, &parse_line/1)
-  end
-
-  defp parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(2, &String.to_integer/1)
+    filename
+    |> Parser.from_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+  defp sum_values([id, _dish, price], report), do: Map.put(report, id, price + report[id])
 end
