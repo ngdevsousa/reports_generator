de2bb72e0d7770c3c0d978b35a1c4a2a4c23d500
f.sousa <ng.dev.sousa@outlook.com>
2021-03-11 19:57:29 -0300

    Add reduce

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -2,8 +2,11 @@ defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
     |> File.stream!()
-    # |> Enum.map(fn line -> parse_line(line) end)
-    |> Enum.map(&parse_line/1)
+    |> Enum.reduce(report_acc(), fn line, report ->
+      [id, _dish, price] = parse_line(line)
+      Map.put(report, id, price + report[id])
+    end)
+    # |> Enum.reduce(%{}, &parse_line/1)
   end
 
   defp parse_line(line) do
@@ -12,4 +15,6 @@ defmodule ReportsGenerator do
     |> String.split(",")
     |> List.update_at(2, &String.to_integer/1)
   end
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
