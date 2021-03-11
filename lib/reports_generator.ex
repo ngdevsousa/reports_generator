57322357d80ff198ff5689eb77399f652eadffc8
f.sousa <ng.dev.sousa@outlook.com>
2021-03-10 22:12:14 -0300

    Add report files with build

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,5 +1,11 @@
 defmodule ReportsGenerator do
-  def hello do
-    :world
+  def build(filename) do
+    "reports/#{filename}"
+    |> File.read()
+    |> handle_file()
   end
+
+  defp handle_file({:ok, result}), do: result
+
+  defp handle_file({:error, reason}), do: reason
 end
