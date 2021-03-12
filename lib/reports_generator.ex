ed7ca2e908442d166aaa570ec044221468cbb9f5
f.sousa <ng.dev.sousa@outlook.com>
2021-03-11 21:19:45 -0300

    Updated to include dish metrics

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,15 +1,42 @@
 alias ReportsGenerator.Parser
 
 defmodule ReportsGenerator do
+  @available_dishs [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @kinds ["dishs", "users"]
+
   def build(filename) do
     filename
     |> Parser.from_file()
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
-  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_k, v} -> v end)
+  def fetch_higher_cost(report, kind) when kind in @kinds do
+    {:ok, Enum.max_by(report[kind], fn {_k, v} -> v end)}
+  end
 
-  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+  def fetch_higher_cost(_report, _kind), do: {:error, "Invalid kind!"}
 
-  defp sum_values([id, _dish, price], report), do: Map.put(report, id, price + report[id])
+  defp report_acc do
+    dishs = Enum.into(@available_dishs, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "dishs" => dishs}
+  end
+
+  defp sum_values([id, dish_name, price], %{"dishs" => dishs, "users" => users} = report) do
+    users = Map.put(users, id, price + users[id])
+    dishs = Map.put(dishs, dish_name, dishs[dish_name] + 1)
+
+    %{report | "users" => users, "dishs" => dishs}
+  end
 end
