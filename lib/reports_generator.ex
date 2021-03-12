ddaa7d6672f2e9c977a4412d68bb10b35d0d0e91
f.sousa <ng.dev.sousa@outlook.com>
2021-03-12 19:52:48 -0300

    Add build_bulk with improvements

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -20,6 +20,12 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_bulk(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
   def fetch_higher_cost(report, kind) when kind in @kinds do
     {:ok, Enum.max_by(report[kind], fn {_k, v} -> v end)}
   end
@@ -30,13 +36,29 @@ defmodule ReportsGenerator do
     dishs = Enum.into(@available_dishs, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{"users" => users, "dishs" => dishs}
+    build_report_map(dishs, users)
   end
 
-  defp sum_values([id, dish_name, price], %{"dishs" => dishs, "users" => users} = report) do
+  defp sum_values([id, dish_name, price], %{"dishs" => dishs, "users" => users}) do
     users = Map.put(users, id, price + users[id])
     dishs = Map.put(dishs, dish_name, dishs[dish_name] + 1)
 
-    %{report | "users" => users, "dishs" => dishs}
+    build_report_map(dishs, users)
   end
+
+  defp sum_reports(%{"dishs" => new_dishs, "users" => new_users}, %{
+         "dishs" => final_dishs,
+         "users" => final_users
+       }) do
+    dishs = merge_maps(new_dishs, final_dishs)
+    users = merge_maps(new_users, final_users)
+
+    build_report_map(dishs, users)
+  end
+
+  defp merge_maps(new_map, final_map) do
+    Map.merge(new_map, final_map, fn _k, new_value, final_value -> new_value + final_value end)
+  end
+
+  defp build_report_map(dishs, users), do: %{"dishs" => dishs, "users" => users}
 end
